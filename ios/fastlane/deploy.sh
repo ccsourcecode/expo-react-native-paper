47f3d1ad07841494548f33bc4f15160e160acc68
Cedric_WS <cedric_ws@email.com>
2025-05-20 14:22:32 +0800

    x

--- ios/fastlane/deploy.sh
+++ ios/fastlane/deploy.sh
@@ -3,4 +3,4 @@
 echo ${IOS_PRIVATE_KEY_BASE64} | base64 -d > PrivateKey.p12
 echo ${APP_STORE_CONNECT_API_KEY_BASE64} | base64 -d > AppStoreConnectApiKey.p8
 
-fastlane beta
+bundle exec fastlane beta
