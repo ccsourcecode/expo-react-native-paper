da201739382080b56c2604f2d4d858d76c81c06a
Cedric_WS <cedric_ws@email.com>
2025-05-20 22:33:47 +0800

    x

--- android/fastlane/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo ${ANDROID_SIGNING_KEY_BASE64} | base64 -d > fastlane/example-app.keystore
-echo ${ANDROID_PLAY_STORE_CREDENTIALS_BASE64} | base64 -d > fastlane/play-store-credentials.json
-
-fastlane beta
--- ios/fastlane/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo ${IOS_PRIVATE_KEY_BASE64} | base64 -d > distribution.p12
-echo ${APP_STORE_CONNECT_API_KEY_BASE64} | base64 -d > AppStoreConnectApiKey.p8
-
-bundle exec fastlane beta
